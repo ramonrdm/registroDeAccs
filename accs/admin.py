853114533e29fbb6ad63bcfe1cb143339a936205
antonio <antoniobesen@hotmail.com>
2017-05-03 16:40:03 -0300

    Adicionados campos de busca para Acc e alunos

--- accs/admin.py
+++ accs/admin.py
@@ -4,5 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 from models import Acc, Aluno
 
-admin.site.register(Acc)
-admin.site.register(Aluno)
+class AccAdmin(admin.ModelAdmin):
+	search_fields = ['titulo']
+
+
+class AlunoAdmin(admin.ModelAdmin):
+	search_fields = ['nome']
+
+
+
+admin.site.register(Acc, AccAdmin)
+admin.site.register(Aluno, AlunoAdmin)
